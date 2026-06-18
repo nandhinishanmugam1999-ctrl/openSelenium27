a7a762dfe8bfd661ce23417f0a051cb4c9cfb2ae
srivi <srivi@LAPTOP-ADPTUOBU.mshome.net>
2026-06-18 11:04:57 +0530

    logout

---
+++ openSelenium27/src/main/java/ObjectRepositiry/logout.java
@@ -0,0 +1,27 @@
+package ObjectRepositiry;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class logout {
+	
+	WebDriver driver = null;
+	public logout(WebDriver driver) {
+		this.driver= driver;
+		PageFactory.initElements(driver,this);
+	}
+	
+	@FindBy(id="name")
+	private WebElement namebtn;
+	
+	
+	public WebElement getNamebtn() {
+		return namebtn;
+	}
+	
+	
+
+
+}
